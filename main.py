fee6090076586f666c1cc9cfe995a3d7d93ee16d
zhangyunping <yp000925@outlook.com>
2024-06-15 14:48:34 +0800

    modified

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 '''
-This script is used to validate the PadDH with your own data
-Please follow the readme.md for details
+This script is used to validate the PadDH with experimental data
+Two examples are provided: USAF and convallaria
+You may use your own experimental data by defining your own task within the parse_task function
 '''
 from functools import partial
 import os
@@ -12,13 +13,14 @@ import json
 from torch.fft import fft2, ifft2
 import torch
 import matplotlib.pyplot as plt
+from pad.diffusion import create_sampler
+from pad.condition_methods import GradientCorrection,Identity
 from pad.unet import create_model
-from pad.physical_model import DHOperator
-from data.dataloader import get_dataset, get_dataloader
+from pad.physical_model import DHOperator, get_noise
 from pad.utils import rgb_to_gray_tensor,get_logger,prepross_bg,generate_otf_torch
 import torchvision.transforms as transforms
-from pad.physical_model import DHCorrector
-from pad.script_util import create_gaussian_diffusion
+from data.dataloader import get_dataset, get_dataloader
+
 def load_yaml(file_path: str) -> dict:
     with open(file_path) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
@@ -34,7 +36,6 @@ def psnr(x, im_orig):
     mse = torch.mean(torch.square(im_orig - x))
     psnr = torch.tensor(10.0) * torch.log10(1 / mse)
     return psnr
-
 
 
 def main():
@@ -64,61 +65,61 @@ def main():
     task_config = load_yaml(args.task_config)
     EXP_NAME = args.exp_name
 
-    # Working directory
-    out_path = os.path.join(args.save_dir, EXP_NAME)
-    os.makedirs(out_path, exist_ok=True)
-    for img_dir in ['input', 'recon', 'progress', 'label']:
-        os.makedirs(os.path.join(out_path, img_dir), exist_ok=True)
 
     # Load model
     model = create_model(**model_config)
     model = model.to(device)
     model.eval()
 
-    # Prepare Operator
+    # Prepare Operator and noise
     operator = DHOperator(device=device, **task_config['operator'])
-    # noiser = get_noise(**task_config['operator']['noise'])
-    logger.info(f"Prop kernel: {task_config['operator']['prop_kernel']} ")
+    noiser = get_noise(**task_config['operator']['noise'])
+    logger.info(f"Prop kernel: {task_config['operator']['prop_kernel']} / Noise: {task_config['operator']['noise']['name']}"
+                f" / Noise level: {task_config['operator']['noise']['sigma']}")
+
+    # Prepare conditioning method
+    scale = task_config['conditioning']['scale']
+    if task_config['conditioning']['method'] == 'gc':
+        logger.info(f"Conditioning method : sampling with gradient correction")
+        logger.info(f"Conditioning scale : {scale}")
+        cond_method = GradientCorrection(operator=operator, noiser=noiser, scale=scale)
+    else:
+        logger.info(f"Conditioning method : vanilla sampling")
+        cond_method = Identity(operator=operator, noiser=noiser, scale=scale)
+    measurement_cond_fn = cond_method.conditioning
+
+    logger.info(f"Conditioning method : {task_config['conditioning']['method']}")
+
+    # Load diffusion sampler
+    diffusion_config['timestep_respacing'] = [500]
+    sampler = create_sampler(**diffusion_config)
+    sample_fn = partial(sampler.p_sample_loop, model=model, measurement_cond_fn=measurement_cond_fn)
+
+    # Working directory
+    out_path = os.path.join(args.save_dir, EXP_NAME)
+    os.makedirs(out_path, exist_ok=True)
+    for img_dir in ['input', 'recon', 'progress', 'label']:
+        os.makedirs(os.path.join(out_path, img_dir), exist_ok=True)
+
     # # Prepare data
     data_config = task_config['data']
     transform = transforms.Compose([transforms.ToTensor()])
     dataset = get_dataset(**data_config, transforms=transform)
     loader = get_dataloader(dataset, batch_size=1, num_workers=0, train=False)
 
-    for i,measurement in enumerate(loader):
+    for i, measurement in enumerate(loader):
         logger.info(f"Inference for image {i}")
         fname = str(i).zfill(5) + '.png'
-        measurement = measurement.to(device)
         measurement = measurement / torch.max(measurement)
-
-        # Prepare correcting method
-        scale = task_config['correction']['scale']
-        corrector = DHCorrector(operator=operator, measurement=measurement,scale=scale)
-
-        # Load diffusion sampler
-        diffusion_config['timestep_respacing'] = [500]
-        diffusion = create_gaussian_diffusion(**diffusion_config)
-        sample_fn = diffusion.p_sample_loop
-
-        # Sampling
+        measurement = measurement.to(device)
         x_start = operator.backward(measurement).requires_grad_()
-        plt.imshow(x_start[0,0,:,:].tolist(), cmap='gray')
+        plt.imshow(x_start[0, 0, :, :].tolist(), cmap='gray')
         plt.show()
 
-        sample = sample_fn(
-            model,
-            x_start.shape,
-            x_start = x_start,
-            correction_fn=corrector.correcting,
-            save_root=out_path,
-            verbose = True,
-            progress=True
-        )
-
+        # Sampling
+        sample = sample_fn(x_start=x_start, measurement=measurement, record=True, save_root=out_path)
         plt.imsave(os.path.join(out_path, 'input', fname), rgb_to_gray_tensor(measurement)[0, 0, :, :].tolist(), cmap='gray')
         plt.imsave(os.path.join(out_path, 'recon', fname), rgb_to_gray_tensor(sample)[0, 0, :, :].tolist(), cmap='gray')
-
-
 
 if __name__ == '__main__':
     main()
